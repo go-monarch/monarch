17d3573c450b772b9ca9819752d271138ff07836
neghi14 <sambasseynengimote@gmail.com>
2025-03-04 02:07:24 +0100

    update: fixed nil dereference error when change database schema

--- collection.go
+++ collection.go
@@ -281,96 +281,97 @@ func (c *Collection[T]) unMarshal(doc bson.D) (*T, error) {
 	}
 	r := reflect.ValueOf(&result)
 	for _, e := range doc {
-		if e.Key != "_id" {
-			f := s.FieldByDBName[e.Key]
-			switch e.Value.(type) {
-			case bson.A:
-				val := e.Value.(bson.A)
-				newVal := reflect.MakeSlice(f.FieldType, 0, 0)
-				for _, v := range val {
-					newVal = reflect.Append(newVal, reflect.ValueOf(v))
-				}
-				f.ReflectValueOf(ctx, r).Set(newVal)
-			case bson.Binary:
-				if val, ok := e.Value.(bson.Binary); ok {
-					if val.Subtype == 4 {
-						id, err := uuid.FromBytes(val.Data)
-						if err != nil {
-							return nil, err
-						}
-						f.ReflectValueOf(ctx, r).Set(reflect.ValueOf(id))
-					}
-				}
-			case bson.ObjectID:
-				if val, ok := e.Value.(bson.ObjectID); ok {
-					f.ReflectValueOf(ctx, r).Set(reflect.ValueOf(val.Hex()))
-				}
-			case bson.DateTime:
-				if val, ok := e.Value.(bson.DateTime); ok {
-					f.ReflectValueOf(ctx, r).Set(reflect.ValueOf(val.Time()))
-				}
-			case bson.D:
-				switch f.FieldType.Kind() {
-				case reflect.Map:
-					val := e.Value.(bson.D)
-					newMap := reflect.MakeMap(f.FieldType)
+		for _, f := range s.Fields {
+			if e.Key == f.DBName {
+				switch e.Value.(type) {
+				case bson.A:
+					val := e.Value.(bson.A)
+					newVal := reflect.MakeSlice(f.FieldType, 0, 0)
 					for _, v := range val {
-						newMap.SetMapIndex(reflect.ValueOf(v.Key), reflect.ValueOf(v.Value))
+						newVal = reflect.Append(newVal, reflect.ValueOf(v))
 					}
-					f.ReflectValueOf(ctx, r).Set(newMap)
+					f.ReflectValueOf(ctx, r).Set(newVal)
+				case bson.Binary:
+					if val, ok := e.Value.(bson.Binary); ok {
+						if val.Subtype == 4 {
+							id, err := uuid.FromBytes(val.Data)
+							if err != nil {
+								return nil, err
+							}
+							f.ReflectValueOf(ctx, r).Set(reflect.ValueOf(id))
+						}
+					}
+				case bson.ObjectID:
+					if val, ok := e.Value.(bson.ObjectID); ok {
+						f.ReflectValueOf(ctx, r).Set(reflect.ValueOf(val.Hex()))
+					}
+				case bson.DateTime:
+					if val, ok := e.Value.(bson.DateTime); ok {
+						f.ReflectValueOf(ctx, r).Set(reflect.ValueOf(val.Time()))
+					}
+				case bson.D:
+					switch f.FieldType.Kind() {
+					case reflect.Map:
+						val := e.Value.(bson.D)
+						newMap := reflect.MakeMap(f.FieldType)
+						for _, v := range val {
+							newMap.SetMapIndex(reflect.ValueOf(v.Key), reflect.ValueOf(v.Value))
+						}
+						f.ReflectValueOf(ctx, r).Set(newMap)
+					default:
+						continue
+					}
+				case int, int8, int16, int32, int64:
+					var val int64
+					switch e.Value.(type) {
+					case int:
+						val = int64(e.Value.(int))
+					case int8:
+						val = int64(e.Value.(int8))
+					case int16:
+						val = int64(e.Value.(int16))
+					case int32:
+						val = int64(e.Value.(int32))
+					default:
+						val = int64(e.Value.(int64))
+					}
+					f.ReflectValueOf(ctx, r).SetInt(val)
+				case uint, uint8, uint16, uint32, uint64:
+					var val uint64
+					switch e.Value.(type) {
+					case uint:
+						val = uint64(e.Value.(uint))
+					case uint8:
+						val = uint64(e.Value.(uint8))
+					case uint16:
+						val = uint64(e.Value.(uint16))
+					case uint32:
+						val = uint64(e.Value.(uint32))
+					default:
+						val = uint64(e.Value.(uint64))
+					}
+					f.ReflectValueOf(ctx, r).SetUint(val)
+				case float32, float64:
+					var val float64
+					switch e.Value.(type) {
+					case uint:
+						val = float64(e.Value.(float32))
+					default:
+						val = float64(e.Value.(float64))
+					}
+					f.ReflectValueOf(ctx, r).SetFloat(val)
+				case bool:
+					val := e.Value.(bool)
+					f.ReflectValueOf(ctx, r).SetBool(val)
+				case string:
+					val := e.Value.(string)
+					f.ReflectValueOf(ctx, r).SetString(val)
 				default:
-					continue
+					f.ReflectValueOf(ctx, r).Set(reflect.ValueOf(e.Value))
 				}
-			case int, int8, int16, int32, int64:
-				var val int64
-				switch e.Value.(type) {
-				case int:
-					val = int64(e.Value.(int))
-				case int8:
-					val = int64(e.Value.(int8))
-				case int16:
-					val = int64(e.Value.(int16))
-				case int32:
-					val = int64(e.Value.(int32))
-				default:
-					val = int64(e.Value.(int64))
-				}
-				f.ReflectValueOf(ctx, r).SetInt(val)
-			case uint, uint8, uint16, uint32, uint64:
-				var val uint64
-				switch e.Value.(type) {
-				case uint:
-					val = uint64(e.Value.(uint))
-				case uint8:
-					val = uint64(e.Value.(uint8))
-				case uint16:
-					val = uint64(e.Value.(uint16))
-				case uint32:
-					val = uint64(e.Value.(uint32))
-				default:
-					val = uint64(e.Value.(uint64))
-				}
-				f.ReflectValueOf(ctx, r).SetUint(val)
-			case float32, float64:
-				var val float64
-				switch e.Value.(type) {
-				case uint:
-					val = float64(e.Value.(float32))
-				default:
-					val = float64(e.Value.(float64))
-				}
-				f.ReflectValueOf(ctx, r).SetFloat(val)
-			case bool:
-				val := e.Value.(bool)
-				f.ReflectValueOf(ctx, r).SetBool(val)
-			case string:
-				val := e.Value.(string)
-				f.ReflectValueOf(ctx, r).SetString(val)
-			default:
-				f.ReflectValueOf(ctx, r).Set(reflect.ValueOf(e.Value))
 			}
-
 		}
+
 	}
 	return &result, nil
 }
